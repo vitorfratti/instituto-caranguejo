5b8aecd9ed4c5672aa03d905bf727be7e5750af5
vitorhangar <vitor@hangar.digital>
2024-06-27 19:57:37 -0300

    :art: [ DB ] Banco atualizado

--- instituto-caranguejo.sql
+++ instituto-caranguejo.sql
@@ -28,11 +28,12 @@ CREATE TABLE IF NOT EXISTS `activities` (
   `date` date DEFAULT NULL,
   `project_id` bigint(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Copiando dados para a tabela instituto-caranguejo.activities: ~1 rows (aproximadamente)
+-- Copiando dados para a tabela instituto-caranguejo.activities: ~2 rows (aproximadamente)
 INSERT INTO `activities` (`id`, `name`, `slug`, `link`, `date`, `project_id`) VALUES
-	(10, 'Atividade 1', 'atividade-1', '', '2024-06-24', 40);
+	(10, 'Atividade 1', 'atividade-1', 'Atividade 1 Link', '2024-06-24', 40),
+	(13, 'Teste', 'teste', '', '2024-06-27', 41);
 
 -- Copiando estrutura para tabela instituto-caranguejo.projects
 CREATE TABLE IF NOT EXISTS `projects` (
@@ -43,18 +44,12 @@ CREATE TABLE IF NOT EXISTS `projects` (
   `link` varchar(100) DEFAULT NULL,
   `date` date DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=52 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=53 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Copiando dados para a tabela instituto-caranguejo.projects: ~8 rows (aproximadamente)
+-- Copiando dados para a tabela instituto-caranguejo.projects: ~2 rows (aproximadamente)
 INSERT INTO `projects` (`id`, `name`, `slug`, `description`, `link`, `date`) VALUES
-	(40, 'Projeto Teste', 'projeto-teste', 'Teste', 'Teste', '2024-06-16'),
-	(41, 'Projeto Teste 2', 'projeto-teste-2', 'Teste', 'Teste', '2024-06-16'),
-	(42, 'Projeto Teste 3', 'projeto-teste-3', 'Projeto Teste 3 Descrição', 'Teste', '2024-06-16'),
-	(43, 'Projeto Teste 4', 'projeto-teste-4', 'Teste', 'Teste', '2024-06-16'),
-	(44, 'Projeto Teste 5', 'projeto-teste-5', 'Teste', 'Teste', '2024-06-16'),
-	(45, 'Projeto Teste 6', 'projeto-teste-6', 'Teste', 'Teste', '2024-06-16'),
-	(46, 'Projeto Teste 7', 'projeto-teste-7', 'Teste', 'Teste', '2024-06-16'),
-	(47, 'Projeto Teste 8', 'projeto-teste-8', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.', 'Teste', '2024-06-16');
+	(40, 'Projeto 1', 'projeto-1-1', 'Projeto 1 Descrição', 'Projeto 1 Link', '2024-06-16'),
+	(41, 'Projeto 2', 'projeto-2-1', 'Projeto 2 Descrição', 'Projeto 2 Link', '2024-06-16');
 
 -- Copiando estrutura para tabela instituto-caranguejo.student_activities
 CREATE TABLE IF NOT EXISTS `student_activities` (
@@ -63,13 +58,16 @@ CREATE TABLE IF NOT EXISTS `student_activities` (
   `activity_id` bigint(100) DEFAULT NULL,
   `score` decimal(65,0) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Copiando dados para a tabela instituto-caranguejo.student_activities: ~3 rows (aproximadamente)
+-- Copiando dados para a tabela instituto-caranguejo.student_activities: ~6 rows (aproximadamente)
 INSERT INTO `student_activities` (`id`, `user_id`, `activity_id`, `score`) VALUES
 	(15, 49, 10, 9),
 	(17, 50, 10, NULL),
-	(18, 51, 10, 6);
+	(18, 51, 10, 6),
+	(20, 49, 13, 8),
+	(22, 51, 13, NULL),
+	(25, 55, 10, NULL);
 
 -- Copiando estrutura para tabela instituto-caranguejo.users
 CREATE TABLE IF NOT EXISTS `users` (
@@ -86,14 +84,14 @@ CREATE TABLE IF NOT EXISTS `users` (
   `role` bigint(100) DEFAULT NULL,
   `approved` int(100) DEFAULT 0,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=52 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=57 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Copiando dados para a tabela instituto-caranguejo.users: ~3 rows (aproximadamente)
+-- Copiando dados para a tabela instituto-caranguejo.users: ~4 rows (aproximadamente)
 INSERT INTO `users` (`id`, `name`, `email`, `phone`, `registration_number`, `course`, `year`, `institution`, `hours_to_be_validated`, `password`, `role`, `approved`) VALUES
-	(21, 'Vitor Admin', 'vitor@admin.com', NULL, NULL, NULL, NULL, NULL, NULL, '$2y$10$pj2EoVbHd0KR.shprQZUluQlLYxxPnt5vkus3ddJd2ZYyZXaIO2Ge', 1, 1),
-	(48, 'Vitor Funcionário', 'vitor@funcionario.com', NULL, NULL, NULL, NULL, NULL, NULL, '$2y$10$KcBwqBm0aIEIbLgqKi/s7.hua3i83uijPpHvH6M5QF5K.pv3TEhna', 2, 1),
-	(49, 'Vitor Aluno', 'vitor@aluno.com', '(11) 1 1111-1111', 11111111111111, 'Engenharia de Software', '2', 'Católica SC', 100, '$2y$10$9LqvTxMZ9V6mu2rQmWgClujJslvOhonCpI/N66oIlwNEK7i4KDm0u', 3, 0),
-	(51, 'Vitor Aluno 2', 'vitoraluno2@gmail.com', '(11) 1 1111-1111', 111111, 'Engenharia de Software', '2', 'Católica SC', 100, '$2y$10$43t113PXXhaWdW/gq7ULReK0WiGcVLoxPpEztww8iDgBgmmKGLNHO', 3, 1);
+	(21, 'PAC Admin', 'pac@admin.com', NULL, NULL, NULL, NULL, NULL, NULL, '$2y$10$9g9ZiW.j3I.kPbt0rduPtukQKlIvTWO.P3kkV6oNkPaxorqXKit32', 1, 1),
+	(54, 'Instituto Caranguejo', 'institutocaranguejo@admin.com', NULL, NULL, NULL, NULL, NULL, NULL, '$2y$10$JhZrqB0dJBlNKjWtuCsN3OP3R37bvR8IN1GvvkhRnM.lApiQD0KeO', 1, 1),
+	(55, 'Aluno Exemplo', 'aluno@exemplo.com', '(11) 1 1111-1111', 111111111, 'Engenharia de Software', '2', 'Católica SC', 100, '$2y$10$Qa5NVoe0Yv5QroMiqVsC8.y6SBpyD0/mJ8vqoMubGxOa/vxsLzSdS', 3, 0),
+	(56, 'Funcionário Exemplo', 'funcionario@exemplo.com', NULL, NULL, NULL, NULL, NULL, NULL, '$2y$10$TtTozIOKHTauzrcyEii4sufoptX9i51pZxqYUP8igGzZr5FGFKeEe', 2, 0);
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
